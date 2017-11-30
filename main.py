94accc48bb4793f7e66d4d46f34ba9d884e537fc
Gonçalo Raposo <gonced_8@hotmail.com>
2017-11-30 12:54:57 +0000

    Prediction

--- main.py
+++ main.py
@@ -34,12 +34,12 @@ x_test = x_test.astype('float32')/255.0
 features = model_cnn.predict(x_test[:, : , : , :])
 features.shape
 
-sequence = np.zeros((x_test.shape[0], 10, 512))  #15000 sequences and 10 steps per sequence and 512 features
+sequence = np.zeros((x_test.shape[0], 20, 512))  #15000 sequences and 10 steps per sequence and 512 features
 sequence.shape
 
 for i in range(sequence.shape[0]):
     for j in range(sequence.shape[1]):
-        sequence[i, j] = (features[i, int(j/16) , j%16, :])
+        sequence[i, j] = (features[i, np.random.randint(16) , np.random.randint(16), :])
 
 
 #Training Recurrent Neural Network
@@ -48,7 +48,7 @@ for i in range(sequence.shape[0]):
     # 1 of the 256 will have the correct prediction, returning the value 1 for the correct and 0 for the others
 
 model_rnn = models.Sequential()
-model_rnn.add(layers.LSTM(64, input_shape=(10, 512)))    #15000 sequences and (example) 10 steps per sequence
+model_rnn.add(layers.LSTM(64, input_shape=(20, 512)))    #15000 sequences and (example) 10 steps per sequence
 model_rnn.add(layers.Dropout(0.2))
 model_rnn.add(layers.Dense(1, activation='sigmoid'))
 model_rnn.compile(loss='mean_squared_error', optimizer='rmsprop')
@@ -64,3 +64,28 @@ model_rnn.fit(sequence, np.ones(sequence.shape[0]), epochs=10, batch_size=64)
     # create 256 sequences by adding a different position in the end of the starting sequence
     # feed the sequences to the RNN and chose 20 with the higher probabilities (output)
     # repeat and stop when the desired size is achieved
+
+random = np.random.randint(x_test.shape[0])
+test_features = model_cnn.predict(x_test[random:random+1, : , : , :])
+
+test_sequence = np.zeros((16*16, 20, 512))
+
+position = np.zeros((16*16, 20))
+
+starting_sequence = np.random.randint(0, 256, 19)
+
+for i in range(sequence.shape[0]):
+    for j in range(starting_sequence.shape[1]):
+        test_sequence[i, j] = test_features[i, int(starting_sequence[i, j]/16), starting_sequence[i,j]%16, :]
+        position[i, j] = starting_sequence[i, j]
+
+    test_sequence[i, -1] = test_features[i, int(j/16), j%16, :]
+    position[i, -1] = i
+
+prediction = model_rnn.predict(test_sequence)
+
+print (prediction)
+
+index = np.argmax(prediction)
+
+print ("index=", index, "probability=", prediction[index], "sequence=", position[index])
