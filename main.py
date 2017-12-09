ba1ec9051f2ba22905605bf6f45db53970b4ef08
Gonçalo Raposo <gonced_8@hotmail.com>
2017-12-09 01:36:00 +0000

    update

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import data
 import cnn
 import rnn
 
-(x_train, y_train) = data.get_train()
+(x_train, _) = data.get_train() #
 
-(x_train_p, _, features) = cnn.get_features (x_train)    # gets preprocessed data and corresponding features (output of cnn)
+(x_train_r, _, features) = cnn.get_features (x_train)    # gets resized data, preprocessed data and corresponding features (output of CNN)
 
 model_rnn = rnn.create()
