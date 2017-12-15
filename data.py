3bd0eac896b81b070f1a2c1195cbd94273c507e1
Gonçalo Raposo <gonced_8@hotmail.com>
2017-12-15 01:15:32 +0000

    new data

--- data.py
+++ data.py
@@ -1,8 +1,9 @@
-from keras.datasets import cifar10
 import numpy as np
+from keras.datasets import cifar10
+from keras.preprocessing import image
+import os
 
-
-def get_train():
+def get_train_cifar10():
 
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -13,9 +14,42 @@ def get_train():
     x = np.empty((0, x_test.shape[1], x_test.shape[2], x_test.shape[3]), dtype=int)
     y = np.empty((0, 1), dtype=int)
 
+    x_train[0].shape
     for i in range (y_test.shape[0]):
         if y_test[i, 0]==chosen_class:
             x = np.append(x, [x_train[i]], axis=0)
             y = np.append(y, [y_train[i]], axis=0)
+
+    return (x, y)
+
+
+def get_train_dogs_vs_cats(choose=2, number=0):
+
+    count=0
+
+    path = './data/dogs vs cats/train'
+
+    x = np.empty((0, 512, 512, 3), dtype=int)
+    y = np.empty((0, 1), dtype=int)
+
+    for name in os.listdir(path):
+
+        if name[0:3]=='cat' and choose==1:
+            continue;
+        if name[0:3]=='dog' and choose==0:
+            continue
+
+        if count==number:
+            break
+        else:
+            count+=1
+
+        img_path = os.path.join(path, name)
+        img = image.load_img(img_path, target_size=(512, 512))
+        array = image.img_to_array(img)
+        array.shape
+        x = np.append(x, [array], axis=0)
+        y = np.append(y, [[int(name[0:3]=='dog')]], axis=0)
+
 
     return (x, y)
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import data
 import cnn
 import rnn
 
-(x_train, _) = data.get_train() #
+(x_train, _) = data.get_train_cifar10() # gets the train data from dataset cifar10
+
+(x_train, _) = data.get_train_dogs_vs_cats(0, 1000) # gets the train data (only cats) from dataset dogs vs cats
 
 (x_train_r, _, features) = cnn.get_features (x_train)    # gets resized data, preprocessed data and corresponding features (output of CNN)
 
